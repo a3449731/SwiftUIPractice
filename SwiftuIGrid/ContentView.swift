dfd1f457c3a5caf47cf4955c34cdf1d21533dd6a
yangxiaobin <381747713@qq.com>
2023-06-21 10:38:20 +0800

    Initial Commit

---
+++ SwiftuIGrid/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftuIGrid
+//
+//  Created by 杨晓斌 on 2023/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
